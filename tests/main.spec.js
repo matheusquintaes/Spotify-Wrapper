24f4cd915deab0f5f5bb1223c6efa7e33eb76ffd
matheusquintaes <matheus.quintaes@gmail.com>
2017-12-28 16:24:19 -0200

    Smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,29 @@
 import { expect } from 'chai';
+import { search, searchAlbuns, searchArtists, searchTracks, searchPlaylists} from '../src/main'
+describe('Spotify Wrapper', () => {
 
-describe('Main', () => {
+  describe('Smoke Tesks', () => {
+
+    it('shoud exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('shoud exist the searchAlbuns method', () => {
+      expect(searchAlbuns).to.exist;
+    });
+
+    it('shoud exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('shoud exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('shoud exist the search method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+
+  });
 
 });
