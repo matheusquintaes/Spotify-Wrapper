2be5de8f1d2980b4ed9eccbb9af0adb69f9d9d81
matheusquintaes <matheus.quintaes@gmail.com>
2018-01-05 15:16:22 -0200

    Bump to version 1.0.3

--- src/config.js
+++ src/config.js
@@ -1,5 +1,2 @@
-const TOKEN_API = 'BQCYSr3o5vYAdtrxrYLUW86CzZBiYqQjfWXyS8EK_2YhwUiwlO4hVZpnT2OFl876FiHPgpFuf2VdS0l58-UvXq0xLs084uE2Ievr39ODSp3HAyk91DW29BbwltvtKn-CGf2haVGCVhin';
-
-export const API_URL = 'https://api.spotify.com/v1';
-
-
+const API_URL = 'https://api.spotify.com/v1';
+export default API_URL;
